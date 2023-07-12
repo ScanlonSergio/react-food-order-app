63ce0dde8b6243fba20e95ff3af2a5bdc2016f77
Scanlon Rebelo <scanlon@edifytech.us>
2023-07-12 21:13:09 +0530

    implementing the food order app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+
+import CartProvider from './store/CartProvider';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+      setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+      setCartIsShown(false);
+  }
+
   return (
-    <div>
-      <h2>Lets get started</h2>
-    </div>
+    <CartProvider>
+        {cartIsShown && <Cart onClose={hideCartHandler} />}
+        <Header onShowCart={showCartHandler} />
+        <main>
+            <Meals />
+        </main>
+    </CartProvider>
   );
 }
 
